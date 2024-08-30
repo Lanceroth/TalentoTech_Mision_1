e4a7d1a45c6e0539f3d02659dd2ec78050d35142
Ruben Sepulveda <43084442+Lanceroth@users.noreply.github.com>
2024-08-29 19:28:03 -0500

    Update Usuario.java

--- GestionDocumento/src/java/com/talentotechg4/gestiondocumento/Usuario.java
+++ GestionDocumento/src/java/com/talentotechg4/gestiondocumento/Usuario.java
@@ -1,4 +1,36 @@
 package java.com.talentotechg4.gestiondocumento;
 
-public class Usuario {
-}
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+
+    class Usuario {
+        private int idUsuario;
+        private String nombre;
+        private String email;
+        private String contraseña;
+        private String tipoUsuario;
+
+        public void registrar() {
+            System.out.println("Registrando usuario: " + nombre);
+        }
+
+        public boolean iniciarSesion() {
+            System.out.println("Iniciando sesión para: " + email);
+            return false;
+        }
+
+        public void cerrarSesion() {
+            System.out.println("Cerrando sesión para: " + email);
+        }
+
+        public void comprarDocumento(Documento doc) {
+            System.out.println("Comprando el documento: " + doc.getTitulo() + " por el usuario: " + nombre);
+        }
+
+        public void alquilarDocumento(Documento doc) {
+            System.out.println("Alquilando el documento: " + doc.getTitulo() + " por el usuario: " + nombre);
+        }
+    }
+
